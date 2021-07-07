3f8d276772819e063f631871bc4ffb42844b29a0
Thomas Cherret <thomas.cherret@papernest.com>
2021-07-07 20:21:33 +0200

    add two functions

---
+++ back/routes/update_timestamp.js
@@ -0,0 +1,18 @@
+const db = require('../db/db.js')
+
+function update_timestamp(id) {
+    const timestamp = Date.now();
+    timestamp = new Date(timestamp);
+
+    db.query('UPDATE USERS set last_connection = $2 where id=$1;', [id, timestamp], (err, result) => {
+        if (err) {
+            console.log(err)
+            return(-1);
+        }
+        else {
+            return(0)
+        }
+      })
+}
+
+module.exports.update_timestamp = update_timestamp;

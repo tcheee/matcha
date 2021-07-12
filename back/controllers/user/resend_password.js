0368d0e842a4a1b3da2a4bd6d812f0ea68702f0f
Thomas Cherret <thomas.cherret@papernest.com>
2021-07-12 22:17:14 +0200

    push

--- back/controllers/user/resend_password.js
+++ back/controllers/user/resend_password.js
@@ -14,7 +14,7 @@ function resend_password(body) {
                 console.log('here')
                 const uuid = result.rows[0].uuid
                 const first_name = result.rows[0].first_name
-                const content = "Hello " + first_name + ", you ask to reinitialize your password for Matcha. Please click on this link to change your password : http://localhost:4200/reset-password/" + uuid
+                const content = "Hello " + first_name + ", you ask to reinitialize your password for Matcha. Please click on this link to change your password : http://localhost:4200/reset-password?uuid=" + uuid
                 const subject = "Hello, please follow the link to reset your password 👋👋👋"
                 send_mail(body.email, subject, content);
                 resolve(0)

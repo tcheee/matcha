84304ee1ef5ec850d953a00ae24db2ad5242c5e7
Viduvern <victor.duvernois2@gmail.com>
2021-07-12 16:20:43 +0200

    ,,

--- front/src/app/services/auth-service.service.ts
+++ front/src/app/services/auth-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpBackend, HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { baseUrl, resetPasswordUrl, registerUrl, imgUrl } from 'src/environments/environment';
+import { baseUrl, resetPasswordUrl, registerUrl} from 'src/environments/environment';
 
 // socket
 import { Socket } from 'ngx-socket-io';

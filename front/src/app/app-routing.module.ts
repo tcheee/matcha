7dbbfdb84440d903eb99efa67e90217df7c3c2b0
Viduvern <victor.duvernois2@gmail.com>
2021-07-12 21:54:27 +0200

    Remove app component automatic routing

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { RegisterComponent } from './components/register/register.component';
 import { ForgetpasswordComponent} from './components/forgetpassword/forgetpassword.component';
 import { ResetpasswordComponent} from './components/resetpassword/resetpassword.component';
 const routes: Routes = [
-  {path: 'authenticate', component: AuthenticateComponent},
+  {path: '', component: AuthenticateComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'forget-password', component: ForgetpasswordComponent},
   {path: 'reset-password', component: ResetpasswordComponent}
--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,17 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'front';
 
-  constructor( 
-    private router : Router
-  ){} 
-  ngOnInit(){
-    this.router.navigate(['/authenticate']);
-  }
+  constructor(){} 
 }

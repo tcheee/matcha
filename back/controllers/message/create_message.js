887877441b5538029b9091891cd1481838b563f5
Thomas Cherret <thomas.cherret@papernest.com>
2021-07-13 21:21:49 +0200

    add messages in db

--- back/controllers/message/create_message.js
+++ back/controllers/message/create_message.js
@@ -1,7 +1,7 @@
-const db = require('../db/db.js')
+const db = require('../../db/db.js')
 
 function create_message(data) {
-    db.query('INSERT INTO inboxes(from_mail, to_mail, content) VALUES($1, $2, $3) RETURNING id;', [data.from_mail, data.to_mail, data.type], (err, result) => {
+    db.query('INSERT INTO inboxes(from_mail, to_mail, content) VALUES($1, $2, $3) RETURNING id;', [data.from, data.to, data.content], (err, result) => {
         if (err) {
             console.log(err)
             return(-1)
--- back/sockets/handleChat.js
+++ back/sockets/handleChat.js
@@ -1,7 +1,10 @@
+const create_message = require('../controllers/message/create_message.js')
+
 module.exports = (io) => {
     const handleChat = function (data) {
-      console.log(data);
-      io.to(data.room).emit('chat', data);
+        console.log(data);
+        io.to(data.room).emit('chat', data);
+        create_message(data);
       }
 
     return {

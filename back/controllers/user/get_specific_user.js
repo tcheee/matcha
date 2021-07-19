af30e2987b7a778c8964647998d44266505669a2
Thomas Cherret <thomas.cherret@papernest.com>
2021-07-19 18:29:55 +0200

    add is_geolocated

--- back/controllers/user/get_specific_user.js
+++ back/controllers/user/get_specific_user.js
@@ -2,7 +2,7 @@ const db = require('../../db/db.js')
 
 function get_specific_user(mail) {
     return new Promise(async (resolve, reject) => {
-        db.query('Select id, mail, first_name, last_name, age, genre, orientation, lat, lng, biography, fame, last_connection, interests from public.users where mail = $1;', [mail], (err, result) => {
+        db.query('Select id, mail, first_name, last_name, age, genre, orientation, lat, lng, is_geolocated, biography, fame, last_connection, interests from public.users where mail = $1;', [mail], (err, result) => {
             if (err) {
                 console.log(err)
                 reject(null)

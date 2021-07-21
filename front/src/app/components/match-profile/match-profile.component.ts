9b548a0a411d8569a3ec377f733e18a68a1ddbb5
Viduvern <victor.duvernois2@gmail.com>
2021-07-21 03:17:41 +0200

    [WIP] Match Profile

--- front/src/app/components/match-profile/match-profile.component.ts
+++ front/src/app/components/match-profile/match-profile.component.ts
@@ -20,11 +20,14 @@ import {
 export class MatchProfileComponent implements OnInit {
 
   selfData$ : Observable<any>;
-  selfData : any;
+  isliked : boolean;
   usersData$ : Observable<any>;
   usersData : any;
   userData : any;
   id : any;
+  image1: any;
+  image2: string;
+  image3: string;
   constructor(
     private _activatedRoute : ActivatedRoute,
     private store$: Store<RootStoreState.RootState>,
@@ -34,7 +37,9 @@ export class MatchProfileComponent implements OnInit {
   ngOnInit(): void {
     const id = this._activatedRoute.snapshot.paramMap.get('id');
     this.selfData$ = this.store$.select(SelfSelectors.getAllStateData);
-    this.selfData$.pipe(first()).subscribe(res => this.selfData = res);
+    this.selfData$.pipe(first()).subscribe(res => {
+      console.log(res);
+    });
     this.usersData$ = this.store$.select(UsersSelector.getAllStateData);
     this.usersData$.pipe(first()).subscribe(res => {
       this.usersData = res.users
@@ -42,7 +47,14 @@ export class MatchProfileComponent implements OnInit {
     this.userData = this.usersData.filter(function(res : any) {
       return res.id == id;
     });
+    // API CALL FOR IMG
+  //  this.image1 = this.userData[0].image1 != "data:image/jpeg;base64," ? this.userData[0].image1 : ""
+ //   this.image2 = this.userData[0].image2 != "data:image/jpeg;base64," ? this.userData[0].image2 : ""
+ //   this.image3 = this.userData[0].image3 != "data:image/jpeg;base64," ? this.userData[0].image3 : ""
     console.log(this.userData);
+    // check if id user in in like self
+    // check if id user is in match self
+    // say that we visit profile of id 
   }
 
 }

0d5e8ab410802c24c8bd5104669bc7d1c862d523
Thomas Cherret <thomas.cherret@papernest.com>
2021-07-26 17:50:12 +0200

    add handleVisit

--- back/controllers/notification/create_visit.js
+++ back/controllers/notification/create_visit.js
@@ -1,16 +1,18 @@
 const db = require('../../db/db.js')
 
 function create_visit(body) {
-    db.query('INSERT INTO visits(from_mail, to_mail) VALUES($1, $2);', [body.from, body.to], (err, result) => {
-        if (err) {
-            console.log(err)
-            return(-1)
-        }
-        else {
-            const ret = result.rows[0]
-            return(0)
-        }
-      })
+    return new Promise(async (resolve, reject) => {
+        db.query('INSERT INTO visits(from_mail, to_mail) VALUES($1, $2);', [body.from, body.to], (err, result) => {
+            if (err) {
+                console.log(err)
+                resolve (-1)
+            }
+            else {
+                const ret = result.rows[0]
+                resolve (0)
+            }
+        })
+    });
 }
 
 module.exports = create_visit;
--- back/sockets/handleVisit.js
+++ back/sockets/handleVisit.js
@@ -1,13 +1,14 @@
 const create_visit = require('../controllers/notification/create_visit.js')
 const create_notification = require('../controllers/notification/create_notification.js')
 
-module.exports = (io) => {
+module.exports = (io, client) => {
+    const { notify } = require("./notifyUser.js")(io, client)
     const handleVisit = function (data) {
-        //find the right client to send the notification
-        //const create_visit = await create_visit(data)
-        //const notification = create_notification(data.from, data.to, 'visit')
         const socket = this;
-        socket.to(data.room).emit('typing', data);
+        data.type = 'visit'
+        const visit = await create_visit(data)
+        const notif = await create_notification(data)
+        notify(data);
       }
 
     return {

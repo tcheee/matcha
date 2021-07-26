e8a3367bc3bd364583c5b0f70d8e1eeb8a80751b
Thomas Cherret <thomas.cherret@papernest.com>
2021-07-26 23:22:37 +0200

    creat block ws

--- back/sockets/handleBlock.js
+++ back/sockets/handleBlock.js
@@ -1,8 +1,10 @@
 const block_user = require('../controllers/user/block_user.js')
 
-module.exports = (io) => {
+module.exports = (io, client) => {
+    const { notify } = require("./notifyUser.js")(io, client)
     const handleBlock = function (data) {
       block_user(data.from, data.to)
+      notify(data)
     }
 
     return {

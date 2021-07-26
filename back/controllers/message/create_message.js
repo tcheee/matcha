b043945ec550e3020a3032f27ed7629fdcde4b0a
Thomas Cherret <thomas.cherret@papernest.com>
2021-07-26 22:27:56 +0200

    correct async

--- back/controllers/message/create_message.js
+++ back/controllers/message/create_message.js
@@ -1,16 +1,18 @@
 const db = require('../../db/db.js')
 
 function create_message(data, bool) {
+    return new Promise((resolve, reject) => {
     db.query('INSERT INTO inboxes(from_mail, to_mail, content, room, seen) VALUES($1, $2, $3, $4, $5) RETURNING id;', [data.from_mail, data.to_mail, data.content, data.room, bool], (err, result) => {
         if (err) {
             console.log(err)
-            return(-1)
+            resolve(-1)
         }
         else {
             const id_user = result.rows[0].id
-            return(id_user)
+            resolve(id_user)
         }
       })
+    })
 }
 
 module.exports = create_message;

3a2cb9e4f7ad71c4ff27b22470d1170887fb34ed
Thomas Cherret <thomas.cherret@papernest.com>
2021-07-27 15:54:22 +0200

    add prod env

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -2,8 +2,18 @@ export const environment = {
   production: true
 };
 
+export const socketURL = 'https://matcha-heroku.herokuapp.com/'
+
 export const baseUrl = 'https://matcha-heroku.herokuapp.com/login'
 export const resetPasswordUrl = 'https://matcha-heroku.herokuapp.com/resend-password/'
 export const registerUrl = 'https://matcha-heroku.herokuapp.com/register'
 export const changePasswordUrl = 'https://matcha-heroku.herokuapp.com/reset-password/'
-export const socketURL = 'https://matcha-heroku.herokuapp.com/'
+
+export const updateUrl = 'https://matcha-heroku.herokuapp.com/update'
+export const orderMessageUrl = 'https://matcha-heroku.herokuapp.com/message-order/'
+export const messageHistoryUrl = 'https://matcha-heroku.herokuapp.com/message-history/'
+export const resetNotifUrl = 'https://matcha-heroku.herokuapp.com/notifications-seen'
+export const resetMessagefUrl = 'https://matcha-heroku.herokuapp.com/messages-seen'
+export const imageUrl = 'https://matcha-heroku.herokuapp.com/all-photos/'
+export const blockUrl = 'https://matcha-heroku.herokuapp.com/block/'
+/*

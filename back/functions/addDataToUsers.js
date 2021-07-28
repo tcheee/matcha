9e771f22598b6ad9b9002299c548430c6e083fcb
Thomas Cherret <thomas.cherret@papernest.com>
2021-07-28 12:25:23 +0200

    add common interest

--- back/functions/addDataToUsers.js
+++ back/functions/addDataToUsers.js
@@ -1,6 +1,7 @@
 const get_first_image = require('../controllers/user/get_first_image.js')
 const get_specific_user = require('../controllers/user/get_specific_user')
 const get_distance = require('./get_distance.js')
+const get_common_interest = require('./get_common_interest.js')
 
 function addDataToUsers(users, requestMail) {
     return new Promise(async (resolve, reject) => {
@@ -9,6 +10,7 @@ function addDataToUsers(users, requestMail) {
             for (elem in users) {
                 users[elem].image = await get_first_image(users[elem].mail)
                 users[elem].distance = requestUser.lat ? await get_distance(requestUser.lat, requestUser.lng, users[elem].lat, users[elem].lng) : null
+                users[elem].common_interest = await get_common_interest(users[elem].interests, requestUser.interests)
             }
             resolve (0)
         } catch (err) {

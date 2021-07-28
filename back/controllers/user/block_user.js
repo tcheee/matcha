0fb7b231e0a5eccbcc1fb7e529b6d8b4089c43be
Thomas Cherret <thomas.cherret@papernest.com>
2021-07-28 18:08:12 +0200

    push debug block

--- back/controllers/user/block_user.js
+++ back/controllers/user/block_user.js
@@ -9,10 +9,12 @@ function block_user(data) {
                 resolve (-1)
             }
             else {
+                console.log(result)
                 const id_user = result.rows[0].id
                 data.likes = -1;
                 data.type = 'unlike'
                 const like = await create_like(data)
+                console.log(like)
                 if (like == -1) {
                     resolve (-1)
                 }

40b72d7ac4c44ddffdb2b75c70cd4cca27ac11b9
Thomas Cherret <thomas.cherret@papernest.com>
2021-07-29 09:55:10 +0200

    correct blocked db

--- back/controllers/user/block_user.js
+++ back/controllers/user/block_user.js
@@ -14,8 +14,6 @@ function block_user(data) {
                 data.likes = -1;
                 data.type = 'unlike'
                 const like = await create_like(data)
-                console.log('result of the suppression of the like')
-                console.log(like) // for vic
                 if (like == -1) {
                     resolve (-1)
                 }

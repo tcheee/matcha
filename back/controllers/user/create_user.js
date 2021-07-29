ed0153db8464998e2bf32505a1977a58838d80ad
Thomas Cherret <thomas.cherret@papernest.com>
2021-07-29 09:54:43 +0200

    push

--- back/controllers/user/create_user.js
+++ back/controllers/user/create_user.js
@@ -29,8 +29,8 @@ function create_user(body, prod_bool) {
             }
         }
         else {
-            //const content = "Hello " + body.firstName + ", you create an account to access Matcha. Please click on this link to active your account : http://localhost:4200/activate-account/" + uuid // LOCAL
-            const content = "Hello " + body.firstName + ", you create an account to access Matcha. Please click on this link to active your account : https://matcha-heroku.herokuapp.com/activate-account/" + uuid   // PROD
+            const content = "Hello " + body.firstName + ", you create an account to access Matcha. Please click on this link to active your account : http://localhost:4200/activate-account/" + uuid // LOCAL
+            //const content = "Hello " + body.firstName + ", you create an account to access Matcha. Please click on this link to active your account : https://matcha-heroku.herokuapp.com/activate-account/" + uuid   // PROD
             const subject = "Hello, please confirm you Matcha account 👋👋👋"
             prod_bool === true ? send_mail(body.email, subject, content) : activate_user(result.rows[0].id)
             resolve (0)
